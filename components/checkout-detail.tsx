b3d81a8b93d7d94d1dd772d98e95cefa5ca4367b
Farid Syafi R <116807763+Faridsyafi@users.noreply.github.com>
2025-12-26 12:39:14 +0700

    Update checkout-detail.tsx

--- components/checkout-detail.tsx
+++ components/checkout-detail.tsx
@@ -7,7 +7,6 @@ import { PaymentButton } from "@/components/payment-button";
 const CheckoutDetail = async ({ reservationId }: { reservationId: string }) => {
   const reservation = await getReservationById(reservationId);
 
-  // payment bisa null, jadi cek yang aman
   if (!reservation || !reservation.payment) {
     return <h1>No Reservation Found</h1>;
   }
@@ -45,7 +44,6 @@ const CheckoutDetail = async ({ reservationId }: { reservationId: string }) => {
           </div>
         </div>
 
-        {/* Payment Button */}
         <PaymentButton reservation={reservation} />
       </div>
 
@@ -56,38 +54,32 @@ const CheckoutDetail = async ({ reservationId }: { reservationId: string }) => {
               <td className="py-2">Reservation ID</td>
               <td className="py-2 text-right truncate">#{reservation.id}</td>
             </tr>
-
             <tr>
               <td className="py-2">Name</td>
               <td className="py-2 text-right">{reservation.user.name}</td>
             </tr>
-
             <tr>
               <td className="py-2">Email</td>
               <td className="py-2 text-right">{reservation.user.email}</td>
             </tr>
-
             <tr>
               <td className="py-2 truncate">Phone Number</td>
               <td className="py-2 text-right truncate">
                 {reservation.user.phone}
               </td>
             </tr>
-
             <tr>
               <td className="py-2">Arrival</td>
               <td className="py-2 text-right truncate">
                 {formatDate(reservation.startDate.toISOString())}
               </td>
             </tr>
-
             <tr>
               <td className="py-2">Departure</td>
               <td className="py-2 text-right truncate">
                 {formatDate(reservation.endDate.toISOString())}
               </td>
             </tr>
-
             <tr>
               <td className="py-2 truncate">Duration</td>
               <td className="py-2 text-right">
@@ -96,14 +88,12 @@ const CheckoutDetail = async ({ reservationId }: { reservationId: string }) => {
                 </span>
               </td>
             </tr>
-
             <tr>
               <td className="py-2 truncate">Amount in Rupiah</td>
               <td className="py-2 text-right truncate">
                 <span>{formatCurrency(reservation.payment.amount)}</span>
               </td>
             </tr>
-
             <tr>
               <td className="py-2">Status</td>
               <td className="py-2 text-right">{reservation.payment.status}</td>
